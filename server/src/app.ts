61f7830c755cf2cac05568d8d3cab13af49cd57b
Yohanna Abana <yohanaabana02@gmail.com>
2024-06-27 12:53:57 +0100

    push

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import path from "path";
 
 const app = express();
+
+app.use(express.static("./src/public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"))
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
